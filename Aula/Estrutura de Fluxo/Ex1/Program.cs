a0fd5f6851ad2790d6b5732ce29bff64989c48bb
Geovanna da S. Lima <71272198+Geovanna-Lima@users.noreply.github.com>
2021-06-16 08:46:45 -0300

    Add files via upload

---
+++ Aula/Estrutura de Fluxo/Ex1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double prova1 = 7;
+            double prova2 = 5;
+            double media = 0;
+            string desempenho;
+
+            media = (prova1 + prova2) / 2;
+
+            if (media <= 5)
+                desempenho = "INSATISFATÓRIO";
+            else if (media <= 7)
+                desempenho = "REGULAR";
+            else if (media <= 8.5)
+                desempenho = "BOM";
+            else
+                desempenho = "ÓTIMO";
+
+            Console.WriteLine("O desempenho do aluno foi " + desempenho);
+        }
+    }
+}
---
+++ Aula/Estrutura de Fluxo/Ex2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcao = "sim";
+
+            switch(opcao)
+            {
+                case "sim":
+                    Console.WriteLine("Você escolheu a opção SIM");
+                    break;
+                case "não":
+                case "talvez":
+                    Console.WriteLine("Você não escolheo a opção SIM");
+                    break;
+                default:
+                    Console.WriteLine("A opção digitada é inválida!");
+                    break;
+            }
+        }
+    }
+}
