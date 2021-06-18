0fa785aec4bba20f6ecf7f1ac83d89f2e88c40d9
Geovanna da S. Lima <71272198+Geovanna-Lima@users.noreply.github.com>
2021-06-18 17:10:26 -0300

    Add files via upload

---
+++ Atividades/Lista2/Ex7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ex7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario;
+            double prestacao;
+            double valor;
+
+            Console.WriteLine("Entre com o valor do seu salário:");
+
+            salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com o valor da prestação do seu empréstimo:");
+
+            prestacao = double.Parse(Console.ReadLine());
+
+            valor = salario * 0.2;
+
+            if (prestacao > valor)
+                Console.WriteLine("Empréstimo não concedido");
+            else
+                Console.WriteLine("Empréstimo concedido");
+        }
+    }
+}
