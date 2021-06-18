9df564ab7895888a51c380f090b1f15fbea3da57
Geovanna da S. Lima <71272198+Geovanna-Lima@users.noreply.github.com>
2021-06-18 12:13:54 -0300

    Add files via upload

---
+++ Atividades/Lista1/Ex5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float numero;
+            float resultado;
+
+            Console.WriteLine("Entre com um número:");
+
+            numero = float.Parse(Console.ReadLine());
+
+            resultado = numero / 5;
+
+            Console.WriteLine("A quita parte desse valor é: " + resultado);
+        }
+    }
+}
