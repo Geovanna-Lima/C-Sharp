ed54e5bb3fa24f77efe9da3a70b407ed1e003d35
Geovanna da S. Lima <71272198+Geovanna-Lima@users.noreply.github.com>
2021-06-18 19:56:00 -0300

    Add files via upload

---
+++ Atividades/Lista2/Ex10/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Ex10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float prova1;
+            float prova2;
+            float prova3;
+            float media;
+
+            Console.WriteLine("Entre com a nota da primeira prova:");
+
+            prova1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com a nota da segunda prova:");
+
+            prova2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com a nota da terceira prova:");
+
+            prova3 = float.Parse(Console.ReadLine());
+
+            media = (((prova1 * 1) + (prova2 * 1) + (prova3 * 2)) / 4) * 10;
+
+            if (media >= 60)
+            {
+                Console.WriteLine("A média do aluno foi:" + media);
+                Console.WriteLine("Aluno aprovado!");
+            }
+            else
+            {
+                Console.WriteLine("A média do aluno foi:" + media);
+                Console.WriteLine("Aluno reprovado!");
+            }
+        }
+    }
+}
