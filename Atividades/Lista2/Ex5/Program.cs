99b3ba2efdd02b73cf8721ae6eaa9f5d7140e22b
Geovanna da S. Lima <71272198+Geovanna-Lima@users.noreply.github.com>
2021-06-18 16:36:40 -0300

    Add files via upload

---
+++ Atividades/Lista2/Ex5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+
+            Console.WriteLine("Entre com um número inteiro:");
+
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com um número inteiro:");
+
+            num2 = int.Parse(Console.ReadLine());
+
+            if (num1 > num2)
+                Console.WriteLine("O número " + num1 + " é maior que o número " + num2);
+            else if (num1 == num2)
+                Console.WriteLine("Os números são iguais" );
+            else
+                Console.WriteLine("O número " + num2 + " é maior que o número " + num1);
+        }
+    }
+}
