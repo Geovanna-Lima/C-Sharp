967bc01c2741991213d1ec8f135bf75f228abd76
Geovanna da S. Lima <71272198+Geovanna-Lima@users.noreply.github.com>
2021-06-18 20:32:07 -0300

    Add files via upload

---
+++ Atividades/Lista2/Ex13/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Ex13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float baseMenor;
+            float baseMaior;
+            float altura;
+            float trapezio;
+
+            Console.WriteLine("Entre com a Base Menor do trapézio:");
+
+            baseMenor = float.Parse(Console.ReadLine());
+
+            if (baseMenor > 0)
+            {
+                Console.WriteLine("Entre com a Base Maior do trapézio:");
+
+                baseMaior = float.Parse(Console.ReadLine());
+
+                if (baseMaior > 0)
+                {
+                    Console.WriteLine("Entre com a Altura do trapézio:");
+
+                    altura = float.Parse(Console.ReadLine());
+
+                    if (altura > 0)
+                    {
+                        trapezio = ((baseMenor + baseMaior) * altura) / 2;
+
+                        Console.WriteLine("A área do Trapézio é: " + trapezio);
+                    }
+                    else
+                        Console.WriteLine("Número Inválido!");
+                }
+                else
+                    Console.WriteLine("Número Inválido!");
+            }
+            else
+                Console.WriteLine("Número Inválido!");
+        }
+    }
+}
