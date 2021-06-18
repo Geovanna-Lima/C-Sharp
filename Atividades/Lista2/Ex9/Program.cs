7e0d41b73e85d315b1cd95bf41b4df70a6876a3e
Geovanna da S. Lima <71272198+Geovanna-Lima@users.noreply.github.com>
2021-06-18 19:19:42 -0300

    Add files via upload

---
+++ Atividades/Lista2/Ex9/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ex9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int soma = 0;
+
+            Console.WriteLine("Entre com um número inteiro maior que zero:");
+
+            num = int.Parse(Console.ReadLine());
+
+            if (num > 0)
+            {
+                while ( num > 0)
+                {
+
+                    soma += num % 10;
+
+                    num /= 10;
+        
+                }
+                Console.WriteLine("A soma dos algorismos desse valor é:" + soma);
+            }
+            else
+                Console.WriteLine("Número Inválido!");
+        }
+    }
+}
