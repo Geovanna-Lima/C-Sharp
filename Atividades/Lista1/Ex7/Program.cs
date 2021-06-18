b01f43b488d2ba3e8a5f2a56f1a46941c5543f7c
Geovanna da S. Lima <71272198+Geovanna-Lima@users.noreply.github.com>
2021-06-18 14:55:50 -0300

    Add files via upload

---
+++ Atividades/Lista1/Ex7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ex7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float c;
+            float f;
+
+            Console.WriteLine("Escreva uma temperatura em graus Fahrenheit:");
+
+            f = float.Parse(Console.ReadLine());
+
+            c = 5 * (f - 32) / 9;
+
+            Console.WriteLine("A temperatura em Celsius é: " + c + "°C");
+        }
+    }
+}
