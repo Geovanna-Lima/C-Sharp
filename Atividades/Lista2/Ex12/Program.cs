189ae63e3aa598afda9c5b7c8b40876a82aab189
Geovanna da S. Lima <71272198+Geovanna-Lima@users.noreply.github.com>
2021-06-18 20:19:32 -0300

    Add files via upload

---
+++ Atividades/Lista2/Ex12/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Ex12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte semana;
+
+            Console.WriteLine("Entre com um número de 1 até 7:");
+
+            semana = byte.Parse(Console.ReadLine());
+
+            switch (semana)
+            {
+                case 1:
+                    Console.WriteLine("Esse número corresponde: Segunda-Feira");
+                    break;
+                case 2:
+                    Console.WriteLine("Esse número corresponde: Terça-Feira");
+                    break;
+                case 3:
+                    Console.WriteLine("Esse número corresponde: Quarta-Feira");
+                    break;
+                case 4:
+                    Console.WriteLine("Esse número corresponde: Quinta-Feira");
+                    break;
+                case 5:
+                    Console.WriteLine("Esse número corresponde: Sexta-Feira");
+                    break;
+                case 6:
+                    Console.WriteLine("Esse número corresponde: Sábado");
+                    break;
+                case 7:
+                    Console.WriteLine("Esse número corresponde: Domingo");
+                    break;
+                default:
+                    Console.WriteLine("Número Inválido!");
+                    break;
+            }
+        }
+    }
+}
