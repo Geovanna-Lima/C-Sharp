b00079102ddcc57c9e0ed2eb4044737e0eb50dc5
Geovanna da S. Lima <71272198+Geovanna-Lima@users.noreply.github.com>
2021-06-18 16:21:21 -0300

    Add files via upload

---
+++ Atividades/Lista2/Ex3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.WriteLine("Entre com um número inteiro:");
+
+            num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+                Console.WriteLine("O número " + num + " é par!");
+            else
+                Console.WriteLine("O número " + num + " é ímpar!");
+        }
+    }
+}
