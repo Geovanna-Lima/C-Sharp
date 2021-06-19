da8c78b9414ac4738a19e44f643f418eb50458d7
Geovanna da S. Lima <71272198+Geovanna-Lima@users.noreply.github.com>
2021-06-18 21:47:47 -0300

    Add files via upload

---
+++ Atividades/Lista2/Ex15/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ex15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte idade;
+            byte tempoServico;
+
+            Console.WriteLine("Qual a sua idade?");
+
+            idade = byte.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual o seu tempo de serviço?");
+
+            tempoServico = byte.Parse(Console.ReadLine());
+
+            if (idade >= 65 || tempoServico >= 30 || (idade >= 60 && tempoServico >= 25))
+                Console.WriteLine("Você pode se aposentar!");
+            else
+                Console.WriteLine("Você ainda não pode se aposentar!");
+        }
+    }
+}
