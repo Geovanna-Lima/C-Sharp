8c48d943cf0fa4e744b0713b5f98d9f3d73a2af9
Geovanna da S. Lima <71272198+Geovanna-Lima@users.noreply.github.com>
2021-06-30 18:22:14 -0300

    Add files via upload

---
+++ Aula/Vetores e Matrizes/Ex4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] dados_alunos = new string[3, 4];
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Lendo informações do " + (i + 1) + "º aluno:");
+
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.WriteLine("Digite dados [" + i + ", " + j +"]: ");
+                    dados_alunos[i, j] = Console.ReadLine();
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.WriteLine("Os dados do " + (i + 1) + "º aluno são:" + dados_alunos[i, j]);
+                }
+            }
+        }
+    }
+}
