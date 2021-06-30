87e27dba9b47e0c4413358b6b82ba79da30d6037
Geovanna da S. Lima <71272198+Geovanna-Lima@users.noreply.github.com>
2021-06-30 11:56:54 -0300

    Add files via upload

---
+++ Atividades/Lista1/Ex8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ex8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double c;
+            double k;
+
+            Console.WriteLine("Escreva uma temperatura em graus Kelvin:");
+
+            k = float.Parse(Console.ReadLine());
+
+            c = k - 273.15;
+
+            Console.WriteLine("A temperatura em Celsius é: " + c + "°C");
+        }
+    }
+}
