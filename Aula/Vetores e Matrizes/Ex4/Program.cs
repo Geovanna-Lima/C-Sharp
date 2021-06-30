5717c2a416949fb30a8cf85133bb069e3fb6701b
Geovanna da S. Lima <71272198+Geovanna-Lima@users.noreply.github.com>
2021-06-30 18:23:12 -0300

    Update Program.cs

--- Aula/Vetores e Matrizes/Ex4/Program.cs
+++ Aula/Vetores e Matrizes/Ex4/Program.cs
@@ -12,7 +12,7 @@ namespace Ex4
             {
                 Console.WriteLine("Lendo informações do " + (i + 1) + "º aluno:");
 
-                for (int j = 0; j < 2; j++)
+                for (int j = 0; j < 4; j++)
                 {
                     Console.WriteLine("Digite dados [" + i + ", " + j +"]: ");
                     dados_alunos[i, j] = Console.ReadLine();
@@ -21,7 +21,7 @@ namespace Ex4
 
             for (int i = 0; i < 3; i++)
             {
-                for (int j = 0; j < 2; j++)
+                for (int j = 0; j < 4; j++)
                 {
                     Console.WriteLine("Os dados do " + (i + 1) + "º aluno são:" + dados_alunos[i, j]);
                 }
