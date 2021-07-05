72cec3164bae8335cf953aae01c150cdfe3ddd87
Geovanna da S. Lima <71272198+Geovanna-Lima@users.noreply.github.com>
2021-07-04 21:01:18 -0300

    Add files via upload

---
+++ Atividades/Lista3/Ex5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int mult = 0;
+
+            Console.WriteLine("Entre com um número inteiro: ");
+            num = int.Parse(Console.ReadLine());
+
+            for (int i = num + 1; mult == 0; i++)
+            {
+                mult = (i % 11 == 0) || (i % 13 == 0) || (i % 17 == 0) ? i : 0;
+            }
+            Console.WriteLine("O primeiro número primo múltiplo de 11, 13 ou 17 é: " + mult);
+        }
+    }
+}
---
+++ Atividades/Lista3/Ex6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int maior = 0;
+            int menor = 0;
+            int x = 0;
+
+            Console.WriteLine("Entre com números inteiros positivos: ");
+
+            do
+            {
+                num = int.Parse(Console.ReadLine());
+                if (x == 0)
+                {
+                    maior = num;
+                    menor = num;
+                    x++;
+                }
+             
+                if (num < 0)
+                {
+                    break;
+                } 
+                else if (num > maior)
+                    maior = num;
+                else if (num < menor)
+                    menor = num;
+            } while (num > 0);
+
+            Console.WriteLine("O maior número é:" + maior);
+            Console.WriteLine("O menor número é:" + menor);
+        }
+    }
+}
