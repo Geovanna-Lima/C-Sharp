59042221ad4d49a0d779f2dc3d8203c1850ca8db
Geovanna da S. Lima <71272198+Geovanna-Lima@users.noreply.github.com>
2021-07-04 21:21:25 -0300

    Add files via upload

---
+++ Atividades/Lista3/Ex7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ex7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int resp;
+
+            Console.WriteLine("Entre com um número inteiro positivo par: ");
+            num = int.Parse(Console.ReadLine());
+
+            if (num > 0 && ((num % 2) == 0))
+            {
+                for (int i = 0; i <= num; i++)
+                {
+                    if (i % 2 == 0) 
+                        Console.WriteLine(i);
+                }
+            }
+            else
+                Console.WriteLine("Número Inválido!");
+        }
+    }
+}
