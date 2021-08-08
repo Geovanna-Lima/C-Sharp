e88ce18658609d1b144d815e3530861eedde73a7
Geovanna da S. Lima <71272198+Geovanna-Lima@users.noreply.github.com>
2021-08-08 18:42:48 -0300

    Add files via upload

---
+++ Aula/Classes/Ex1/Pessoa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ex1
+{
+    class Pessoa
+    {
+        private string nome = "Geovanna";
+        private string sobrenome = "Lima";
+        private byte idade = 18;
+        private float altura = 1.63F;
+        private float peso = 60.0F;
+
+        public string getNome()
+        {
+            return this.nome;
+        }
+    }
+}
+
---
+++ Aula/Classes/Ex1/TestePessoa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    class TestePessoa
+    {
+        static void Main(string[] args )
+        {
+            Pessoa p1 = new Pessoa();
+            Console.WriteLine("O valor dentro do atributo nome é: " + p1.getNome());
+        }
+    }
+}
