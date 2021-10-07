c876f36f8e1bd3f9544de26aba59e4a4656db892
Geovanna da S. Lima <71272198+Geovanna-Lima@users.noreply.github.com>
2021-10-07 19:16:27 -0300

    Add files via upload

---
+++ Aula/Interfaces/Ex1/Quadrado.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    class Quadrado : IForma
+    {
+        private double lado;
+
+        public Quadrado(double lado)
+        {
+            this.lado = lado;
+        }
+
+        public double calcularArea()
+        {
+            return Math.Pow(lado, 2);
+        }
+    }
+}
---
+++ Aula/Interfaces/Ex1/TesteForma.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Ex1
+{
+    class TesteForma
+    {
+        static void mostrarArea(IForma forma)
+        {
+            Console.WriteLine("Área do "  + forma  + ": " + forma.calcularArea() + "\n");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Retângulo");
+            IForma forma = new Retangulo(5.0, 7.5);
+            mostrarArea(forma);
+
+            Console.WriteLine("Círculo");
+            forma = new Circulo(3.5);
+            mostrarArea(forma);
+
+            Console.WriteLine("Quadrado");
+            forma = new Quadrado(10.0);
+            mostrarArea(forma);
+
+            Console.WriteLine("Triângulo");
+            forma = new Triangulo(2.0, 5.5);
+            mostrarArea(forma);
+        }
+    }
+}
